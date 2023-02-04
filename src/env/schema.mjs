9dee8004aff98d7c4fab9956fb63fb0e4fb4c755
Talha Imran <talhaimran284@gmail.com>
2023-02-04 15:06:21 +0500

    Setup google oauth

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -21,6 +21,8 @@ export const serverSchema = z.object({
   ),
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
+  GOGGLE_CLIENT_ID: z.string(),
+  GOGGLE_CLIENT_SECRET: z.string(),
 });
 
 /**
@@ -35,6 +37,8 @@ export const serverEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  GOGGLE_CLIENT_ID: process.env.GOGGLE_CLIENT_ID,
+  GOGGLE_CLIENT_SECRET: process.env.GOGGLE_CLIENT_SECRET,
 };
 
 /**
